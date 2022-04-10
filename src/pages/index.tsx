5fcb8ef71b84a33476260ce47ba20637b368ec5c
Guilherme Zobel <guilherme.zobel@gmail.com>
2022-04-10 18:42:45 -0300

    Feat: Component SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import avatarImg from '../../public/images/avatar.svg';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 
 import styles from './home.module.scss';
@@ -20,6 +21,7 @@ export default function Home() {
           Get acess to all publications <br />
           <span>for $9.90 month</span>
         </p>
+        <SubscribeButton />
       </section>
       <Image src={avatarImg} alt="Girl coding"/>
     </main>
